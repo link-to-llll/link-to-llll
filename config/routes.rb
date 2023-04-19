decfc51fbf88166838ccc797e40e5c1c669e63d3
yutafurunaga <126681269+yutafurunaga@users.noreply.github.com>
2023-04-19 05:11:23 +0000

    サーバー起動準備

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,16 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
-  
+ 
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  scope module: :public do
+   root to: "homes#top"
+   get "home/about"=> 'homes#about'
+  end
+  
+  namespace :admin do
+    root to: "homes#top"
+  
+  end
 end

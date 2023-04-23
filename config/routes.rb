720c95d69eacf30cb18b850d07d21f0e482946ed
Mauve-negi <116134417+Mauve-negi@users.noreply.github.com>
2023-04-23 10:14:05 +0000

    [Update]レイアウトを整えました

--- config/routes.rb
+++ config/routes.rb
@@ -15,9 +15,6 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
-   devise_scope :admin do
-  get 'admin/sign_out' => 'admin/sessions#destroy'
- end
 
   devise_scope :admin do
     get 'admin/sign_out' => 'admin/sessions#destroy'

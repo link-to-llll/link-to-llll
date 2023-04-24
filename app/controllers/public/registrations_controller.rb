2381278b5169ee4040201c8e30cefb271d195309
Mauve-negi <116134417+Mauve-negi@users.noreply.github.com>
2023-04-24 08:28:48 +0000

    ユーザ登録フォーム作成

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -59,4 +59,32 @@ class Public::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+  
+  
+  def create
+    build_resource(sign_up_params)
+    resource.save
+    yield resource if block_given?
+    if resource.persisted?
+      if resource.active_for_authentication?
+        set_flash_message! :notice, :signed_up
+        sign_up(resource_name, resource)
+        respond_with resource, location: after_sign_up_path_for(resource)
+      else
+        set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
+        expire_data_after_sign_in!
+        respond_with resource, location: after_inactive_sign_up_path_for(resource)
+      end
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+      respond_with resource
+    end
+  end
+
+  def sign_up_params
+    params.require(:customer).permit(:email, :password, :password_confirmation, :family_name, :personal_name, :family_name_kana, :personal_name_kana, :post_code, :address, :phone_number)
+  end
+
+  
 end

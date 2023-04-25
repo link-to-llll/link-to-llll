8074c5a5d238815d7feb658a928ce5c6a88e8e2d
EC2 Default User <ec2-user@ip-172-31-29-186.ap-northeast-1.compute.internal>
2023-04-25 09:52:02 +0000

    会員詳細で会員情報の変更を保存するとtop画面に遷移する可能性有り

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -2,16 +2,16 @@ class Admin::CustomersController < ApplicationController
   def index
     @customers = Customer.all
   end
-  
+
   def show
     @customer = Customer.find(params[:id])
     @products = @customer.products.page(params[:page])
   end
-  
+
   def edit
     @customer = Customer.find(params[:id])
   end
-  
+
   def update
     @customer = Customer.find(params[:id])
     if @customer.update(customer_params)
@@ -21,12 +21,12 @@ class Admin::CustomersController < ApplicationController
       render :edit
     end
   end
-  
+
   private
 
   def customer_params
     params.require(:customer).permit(:email, :family_name, :personal_name, :family_name_kana,
       :personal_name_kana, :post_code, :address, :phone_number, :status)
   end
-  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 
   # 顧客用
   # URL /customers/sign_in ...
-  devise_for :customers,skip: [:passwords], controllers: {
+  devise_for :customer,skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
@@ -23,14 +23,22 @@ Rails.application.routes.draw do
     get 'admin/sign_out' => 'admin/sessions#destroy'
   end
 
-  devise_for :users
+  #devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   scope module: :public do
     root to: "homes#top"
     get "home/about"=> 'homes#about'
     resources :products, only: [:show, :index]
+    #get 'customer/edit' => 'customers#edit'
+    #put 'customer' => 'customers#update'
 
+  	resource :customers do
+  		collection do
+  	     get 'quit'
+  	     patch 'out'
+  	  end
+  	 end
     resources :shipping_addresses, only: [:index, :create, :edit, :update, :destroy]
 
     get 'orders/log', to: 'orders#log'

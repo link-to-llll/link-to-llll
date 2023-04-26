a408df33fde99b7740e6f4a7433f1dc95226d1c0
yutafurunaga <126681269+yutafurunaga@users.noreply.github.com>
2023-04-26 10:06:58 +0000

    注文履歴作成

--- app/controllers/admin/order_products_controller.rb
+++ app/controllers/admin/order_products_controller.rb
@@ -1,3 +1,19 @@
 class Admin::OrderProductsController < ApplicationController
-  
+
+  def update
+    @order_product = OrderProduct.find(params[:id])
+    @order = @order_product.order
+    if @order_product.update(order_product_params)
+
+      redirect_to admin_order_path(@order)
+    else
+      render :show
+    end
+  end
+
+  private
+
+  def order_product_params
+    params.require(:order_product).permit(:status)
+  end
 end

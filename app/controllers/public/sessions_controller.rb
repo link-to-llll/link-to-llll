526c13da48de7a8f42fab2e15958db2af4cf81c2
EC2 Default User <ec2-user@ip-172-31-29-186.ap-northeast-1.compute.internal>
2023-04-26 10:06:35 +0000

    ありがとう

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -3,20 +3,8 @@
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
   # Customer.find_by(email: params[:customer][:email])
+  before_action :customer_state, only: [:create]
   
-  # protected
-  # # 退会しているかを判断するメソッド
-  # def customer_state
-  #   ## 【処理内容1】 入力されたemailからアカウントを1件取得
-  #   @customer = Customer.find_by(email: params[:customer][:email])
-  #   ## アカウントを取得できなかった場合、このメソッドを終了する
-  #   return if !@customer
-  #   ## 【処理内容2】 取得したアカウントのパスワードと入力されたパスワードが一致してるかを判別
-  #   if (@customer.valid_password?(params[:customer][:password]) && (@customer.active_for_authentication? == false))
-  #   ## 【処理内容3】
-  #   end
-  # end
-
   # GET /resource/sign_in
   # def new
   #   super
@@ -32,10 +20,24 @@ class Public::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+   protected
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+  
+  
+  # 退会しているかを判断するメソッド
+  def customer_state
+    ## 【処理内容1】 入力されたemailからアカウントを1件取得
+    @customer = Customer.find_by(email: params[:customer][:email])
+    ## アカウントを取得できなかった場合、このメソッドを終了する
+    return if !@customer
+    ## 【処理内容2】 取得したアカウントのパスワードと入力されたパスワードが一致してるかを判別
+    if @customer.valid_password?(params[:customer][:password]) && !@customer.status
+    ## 【処理内容3】
+    redirect_to new_customer_registration_path
+    end
+  end
 end
